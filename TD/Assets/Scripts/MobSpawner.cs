e5044ee2f21eb1de78bae56cc15a4d28426d22f0
AmNI5TIa <56353744+CC-AmNI5TIa@users.noreply.github.com>
2020-10-05 06:25:30 -0700

    Added first logic to spawn enemies

---
+++ TD/Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TD_game
+{
+    public class MobSpawner : MonoBehaviour
+    {
+        [SerializeField] public byte numberOfMobs;
+
+        [SerializeField] private GameObject _enemyPrefab;
+
+        [SerializeField] private byte enemySpawnInterval;
+
+        [SerializeField] private Transform spawnPoint;
+
+        [SerializeField] public List<GameObject> listOfEnemies = new List<GameObject>();
+
+        void Start()
+        {
+            StartCoroutine(routine: Spawner());
+        }
+
+        void Update()
+        {
+
+        }
+        void SpawnAMob()
+        {
+            listOfEnemies.Add(GameObject.Instantiate(_enemyPrefab, spawnPoint.position, Quaternion.identity) as GameObject);
+        }
+        private IEnumerator Spawner()
+        {
+            while (listOfEnemies.Count < numberOfMobs)
+            {
+                SpawnAMob();
+                Debug.Log($"Spawned an enemy");
+                 Debug.Log($"Total number of enemies: {listOfEnemies.Count}");
+                yield return new WaitForSeconds(enemySpawnInterval);
+            }
+        }
+    }
+}
